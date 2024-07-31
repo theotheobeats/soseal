d76581c8213d77d1126f475439bdf4b3f0a23d68
Theo <asakvsa.idn@gmail.com>
2024-07-31 12:03:21 +0700

    navigation bars

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import Navbar from "./Navbar";
 
 export default function Home() {
-  return <main>Front Page</main>;
+  return <main className="h-[200vh] w-full bg-red-50"><div className="w-full"></div></main>;
 }
